975dfbde601449bcc17874c6209c55235f87fbda
Tomáš Ferdan <tom.ferdan@seznam.cz>
2021-03-23 17:20:37 +0100

    Přidáno tlačítko kreslení

--- Tomova_appka.py
+++ Tomova_appka.py
@@ -1,5 +1,4 @@
-#Datum vydání: 12:40 CET  22.03.2021
-#Verze: 1.11
+#Datum poslední úpravy: 23.03.2021
 
 import os
 import subprocess
@@ -9,12 +8,12 @@ import PySimpleGUI as sg
 sg.change_look_and_feel("Topanga")
 
 layout = [[sg.Text ("Co chceš zapnout?"),],
-        [sg.Text(("*********************************************************"), key = 'OUTPUT')],
-        [sg.Button("Slovní hru"),sg.Button("Info"), sg.Button("Opustit")]]
+        [sg.Text(("***************************************************************"), key = 'OUTPUT')],
+        [sg.Button("Slovní hru"),sg.Button("Něco mi nakresli"), sg.Button("Info"), sg.Button("Opustit")]]
 
 window = sg.Window("Tomova appka", layout)
 window1_active = False
-
+window2_active = False
 while True:
     event, values = window.read(timeout=100)
 
@@ -33,14 +32,14 @@ while True:
 
     if not window1_active and event == 'Info':
         window1_active = True
-        answer = "Pro více info kontaktuj 'tom.ferdan@seznamcz'"
+        answer = "Pro více info kontaktuj autora na tom.ferdan@seznamcz"
 
         window['OUTPUT'].update(answer)
 
 
 
 
-        layout1 = [[sg.Text ("Autor: Tomáš Ferdan | Datum vydání: 20:08 CET 22.03.2021 | Verze: 1.11 | Creative Commons")],
+        layout1 = [[sg.Text ("Autor: Tomáš Ferdan | Datum vydání: 17:06 CET 23.03.2021 | Verze: 1.13 | Creative Commons")],
                    [sg.Button('Opustit')]]
 
 
@@ -54,5 +53,33 @@ while True:
         if event1 == sg.WIN_CLOSED or event1 == 'Opustit':
             window1_active  = False
             window1.close()
-            window.close()
-            break
+
+
+    if not window2_active and event == 'Něco mi nakresli':
+        window2_active = True
+
+        layout2 = [[sg.Text ("Co chceš nakreslit?")],
+                    [sg.Text(("***************************************************"), key = 'OUTPUT2')],
+                   [sg.Button('Srdce'), sg.Button("Opustit")]]
+
+
+        window2 = sg.Window("Kreslení", layout2)
+
+
+
+    if window2_active:
+        event2, values2 = window2.read(timeout=100)
+
+        if event2 == sg.WIN_CLOSED or event2 == 'Opustit':
+            window2_active  = False
+            window2.close()
+
+        if event2 == "Srdce" :
+
+            answer2 = "Kreslení spuštěno, měj trpělivost."
+
+            window2['OUTPUT2'].update(answer2)
+
+            command = "srdce"
+
+            subprocess.Popen(command)
